3c882137cdc095c5e8d6ae57c16003c36313d885
Amegh Coder <mintuser65@gmail.com>
2024-04-29 13:37:22 +0530

    updation, add form

--- public/script/main.js
+++ public/script/main.js
@@ -1,8 +1,38 @@
 function init(){
     console.log('hi')
+    const [winWid, winHei] = [window.innerWidth, window.innerHeight];
     const gradHeight = document.querySelector('.gradient-background');
     const frameHeight = document.getElementById('sunglass-frame');
     gradHeight.style.height = `${frameHeight.clientHeight}px`;
+    
+    const navLogoContainer = document.getElementById('nalc');
+    const frameContainer = document.getElementById('frameContainer');
+    frameContainer.style.height = `${winHei - navLogoContainer.clientHeight}px`;
+
+    const frame = document.getElementById('frame');
+    const webLogo = document.getElementById("webLogo");
+    const webLogoName = document.getElementById("webLogoName");
+    let zoomOut, leftAndRight;
+    window.onscroll = (e) => {
+        frame.classList.remove('frame-animation');
+        zoomOut = window.scrollY < 1 ? 1 : (Math.round(window.scrollY));
+        console.log(zoomOut);
+        frame.style.transform = `rotate(-45deg) translateX(${zoomOut}px) translateY(-1px)`;
+
+        // animation on scroll in web logo and name
+        webLogo.classList.remove('cometoleft');
+        webLogoName.classList.remove('cometoright');
+        leftAndRight = window.scrollY / 10;
+        webLogo.style.transform = `translateX(${leftAndRight * 1.35}px)`;
+        webLogoName.style.transform = `translateX(-${leftAndRight * 2.5}px)`;
+        
+    }
+
+    const closePopUpFormBtn = document.getElementById('closePopUp');
+    const formContainer = document.getElementById('form-container');
+    closePopUpFormBtn.addEventListener("click", () => {
+        formContainer.classList.remove("fixedContent");
+    })
 }
 
 document.readyState == 'interactive' ? init() : document.addEventListener('DOMContentLoaded', () => init());
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        theme_gold_2: 'rgba(var(--theme-gold-clr-2) / <alpha-value>)',
+        theme_gold: 'rgba(var(--theme-gold-clr) / <alpha-value>)',
+        theme_blue: 'rgba(var(--theme-blue-clr) / <alpha-value>)',
+      }
+    },
   },
   plugins: [],
 }
